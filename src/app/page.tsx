13d4ad9868d8c77f8f0b91cddb278f504b9b9a68
fredoananda <fredoananda@gmail.com>
2023-07-12 14:43:20 +0800

    article update

---
+++ src/app/page.tsx
@@ -0,0 +1,279 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <header className="py-5 bg-light border-bottom mb-4">
+        <div className="container">
+          <div className="ml-4">
+            <div className="col-lg-6">
+              <div className="card mb-4">
+                <a href="#!">
+                  <img
+                    className="card-img-top"
+                    src="https://dummyimage.com/850x350/dee2e6/6c757d.jpg"
+                    alt="..."
+                  />
+                </a>
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div className="card mb-4">
+                <div className="card-body">
+                  <div className="small text-muted">January 1, 2023</div>
+                  <h2 className="card-title">Featured Post Title</h2>
+                  <p className="card-text text-dark">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a
+                    laboriosam. Dicta expedita corporis animi vero voluptate
+                    voluptatibus possimus, veniam magni quis!
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <div className="grid grid-cols-2 gap-x-4 w-full mt-10">
+        
+
+        <div className="">
+          <div className="row">
+            <div className="ml-4">
+              <div className="grid grid-cols-2 gap-x-4 w-full mt-10 flex-container wrap-container margin-15px ">
+                <div className="flex-item">
+                  <div
+                    className="card mb-4"
+                    style={{ width: "20rem;", border: "4px solid #ccc" }}
+                  >
+                    <a href="#!">
+                      <img
+                        className="card-img-top"
+                        src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg"
+                        alt="..."
+                      />
+                    </a>
+                    <div className="card-body">
+                      <div className="small text-muted">January 1, 2023</div>
+                      <h2 className="card-title h4">Post Title</h2>
+                      <p className="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Reiciendis aliquid atque, nulla.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex-item">
+                  <div
+                    className="card mb-4"
+                    style={{ width: "20rem;", border: "4px solid #ccc" }}
+                  >
+                    <a href="#!">
+                      <img
+                        className="card-img-top"
+                        src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg"
+                        alt="..."
+                      />
+                    </a>
+                    <div className="card-body">
+                      <div className="small text-muted">January 1, 2023</div>
+                      <h2 className="card-title h4">Post Title</h2>
+                      <p className="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Reiciendis aliquid atque, nulla.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="grid grid-cols-2 gap-x-4 w-full mt-10 flex-container wrap-container margin-15px ">
+                <div className="flex-item">
+                  <div
+                    className="card mb-4"
+                    style={{ width: "20rem;", border: "4px solid #ccc" }}
+                  >
+                    <a href="#!">
+                      <img
+                        className="card-img-top"
+                        src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg"
+                        alt="..."
+                      />
+                    </a>
+                    <div className="card-body">
+                      <div className="small text-muted">January 1, 2023</div>
+                      <h2 className="card-title h4">Post Title</h2>
+                      <p className="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Reiciendis aliquid atque, nulla.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex-item">
+                  <div
+                    className="card mb-4"
+                    style={{ width: "20rem;", border: "4px solid #ccc" }}
+                  >
+                    <a href="#!">
+                      <img
+                        className="card-img-top"
+                        src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg"
+                        alt="..."
+                      />
+                    </a>
+                    <div className="card-body">
+                      <div className="small text-muted">January 1, 2023</div>
+                      <h2 className="card-title h4">Post Title</h2>
+                      <p className="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Reiciendis aliquid atque, nulla.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="grid grid-cols-2 gap-x-4 w-full mt-10 flex-container wrap-container margin-15px ">
+                <div className="flex-item">
+                  <div
+                    className="card mb-4"
+                    style={{ width: "20rem;", border: "4px solid #ccc" }}
+                  >
+                    <a href="#!">
+                      <img
+                        className="card-img-top"
+                        src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg"
+                        alt="..."
+                      />
+                    </a>
+                    <div className="card-body">
+                      <div className="small text-muted">January 1, 2023</div>
+                      <h2 className="card-title h4">Post Title</h2>
+                      <p className="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Reiciendis aliquid atque, nulla.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex-item">
+                  <div
+                    className="card mb-4"
+                    style={{ width: "20rem;", border: "4px solid #ccc" }}
+                  >
+                    <a href="#!">
+                      <img
+                        className="card-img-top"
+                        src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg"
+                        alt="..."
+                      />
+                    </a>
+                    <div className="card-body">
+                      <div className="small text-muted">January 1, 2023</div>
+                      <h2 className="card-title h4">Post Title</h2>
+                      <p className="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Reiciendis aliquid atque, nulla.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <nav aria-label="item-center">
+                <hr className="item-center" />
+                <ul className="pagination justify-content-center my-4 flex">
+                  <li className="page-item disabled">
+                    <a className="page-link" href="#" aria-disabled="true">
+                      Newer
+                    </a>
+                  </li>
+                  <li className="page-item active" aria-current="page">
+                    <a className="page-link" href="#!">
+                      1
+                    </a>
+                  </li>
+                  <li className="page-item">
+                    <a className="page-link" href="#!">
+                      2
+                    </a>
+                  </li>
+                  <li className="page-item">
+                    <a className="page-link" href="#!">
+                      3
+                    </a>
+                  </li>
+                  <li className="page-item disabled">
+                    <a className="page-link" href="#!">
+                      ...
+                    </a>
+                  </li>
+                  <li className="page-item">
+                    <a className="page-link" href="#!">
+                      15
+                    </a>
+                  </li>
+                  <li className="page-item">
+                    <a className="page-link" href="#!">
+                      Older
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+        <div
+          className="card-container"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-end",
+          }}
+        >
+          <div
+            className="card"
+            style={{
+              width: "80%",
+              marginTop: "40px",
+              border: "1px solid #ccc",
+              borderRadius: "5px",
+              marginRight: "10px",
+            }}
+          >
+            <div
+              className="card-header"
+              style={{
+                padding: "10px",
+                backgroundColor: "#f8f9fa",
+                fontWeight: "bold",
+                alignItems: "center",
+              
+              }}
+            >
+              TOP ARTICLE
+            </div>
+            <div className="card-body" style={{ padding: "10px" }}>
+              You can put anything you want inside of these side widgets. They
+              are easy to use, and feature the Bootstrap 5 card component!
+            </div>
+            <div className="card-body" style={{ padding: "10px" }}>
+              You can put anything you want inside of these side widgets. They
+              are easy to use, and feature the Bootstrap 5 card component!
+            </div>
+            <div className="card-body" style={{ padding: "10px" }}>
+              You can put anything you want inside of these side widgets. They
+              are easy to use, and feature the Bootstrap 5 card component!
+            </div>
+            <div className="card-body" style={{ padding: "10px" }}>
+              You can put anything you want inside of these side widgets. They
+              are easy to use, and feature the Bootstrap 5 card component!
+            </div>
+            <div className="card-body" style={{ padding: "10px" }}>
+              You can put anything you want inside of these side widgets. They
+              are easy to use, and feature the Bootstrap 5 card component!
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
